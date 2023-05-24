d898e971928085b5956bd87c63ed5cb21279f38a
Ecem <51851879+ecemisildar@users.noreply.github.com>
2023-05-24 10:51:35 +0200

    Delete assignment_2_2022 directory

--- assignment_2_2022/set_permissions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chmod +x scripts/wall_follow_service.py
-chmod +x scripts/bug_as.py
-chmod +x scripts/goalTracker.py
-chmod +x scripts/go_to_point_service.py
-chmod +x scripts/publisher.py
-chmod +x scripts/subscriber.py
-
